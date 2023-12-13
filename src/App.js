1aea22a343261507f800e18577a2d3c883582852
joshmalk <joshmalk03@gmail.com>
2023-12-13 16:04:46 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+    return (
+      <div class="App">
+        <h1>Hello World!</h1>
+        <h2>Welcome To My Card</h2>
+        <img class="card" src="https://media.cnn.com/api/v1/images/stellar/prod/231212131107-01-ronaldo-50-goals.jpg?c=16x9&q=h_833,w_1480,c_fill" alt="Ronaldo" />
+        <div class="paragraph">
+          <p>This is Cristiano Ronaldo. One of the greatest Football/Soccer players ever.</p>
+        </div>
+        <div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button></a></div>
+      </div>
+    );
+  }
 
 export default App;
